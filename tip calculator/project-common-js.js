b6024b7f6014675b76f13d7027c2ef27ae559494
sujaljoisar <sujaljoisar@gmail.com>
2023-06-14 02:12:27 +0530

    tip calc

---
+++ tip calculator/project-common-js.js
@@ -0,0 +1,52 @@
+function _cmnShowErrorMessageBottomOfTheInputField(fieldID, errorMessage) {
+  var inputField = document.getElementById(fieldID);
+  inputField.classList.add("tool-error-border"); //add border
+  inputField.focus(); //focus error field
+
+  var errorMessageElement = document.createElement("p"); //create a p tag for error message
+  errorMessageElement.innerHTML = errorMessage; //set the error message in the p tag
+  errorMessageElement.classList.add("tool-error-message"); //add the error message style class
+  inputField.parentNode.insertBefore(
+    errorMessageElement,
+    inputField.nextSibling
+  ); //set the error message under the error field
+}
+
+function _cmnRemoveAllErrorMessage(){
+    var allErrorBorder = document.getElementsByClassName('tool-error-border');
+    var allErrorMessage = document.getElementsByClassName('tool-error-message');
+    var i;
+
+    //remove border
+    for(i=(allErrorBorder.length)-1;i>=0;i--){
+        allErrorBorder[i].classList.remove('tool-error-border');
+    }
+
+    //remove error message
+    for(i=(allErrorMessage.length)-1;i>=0;i--){
+        allErrorMessage[i].remove();
+    }
+}
+
+function _cmnIsInputFieldEmpty(fieldID){
+    var inputField = document.getElementById(fieldID);
+    if(inputField = "")//check the field empty or not
+    {
+        return true;
+    }
+    return false;
+}
+
+function _cmnHideElement(elementId) {
+  var selectedDisplayValue = document.getElementById(elementId).style.display;
+  if (selectedDisplayValue != "none") {
+    document.getElementById(elementId).style.display = "none";
+  }
+}
+
+function _cmnShowElement(elementId, displayName) {
+  var selectedDisplayValue = document.getElementById(elementId).style.display;
+  if (selectedDisplayValue != displayName) {
+    document.getElementById(elementId).style.display = displayName;
+  }
+}
---
+++ tip calculator/script.js
@@ -0,0 +1,86 @@
+// calculate the tips
+//validate input field
+//show result div
+//hide basic info div
+
+//reset calculator
+//make input field empty
+//hide result div
+//show basic info div
+
+function CalculateTips() {
+  var Bill = document.getElementById("inputBill").value;
+  var Tips = document.getElementById("inputTips").value;
+  var Person = document.getElementById("inputPerson").value;
+
+  if (ValidateTipCalculatorForm(Bill, Tips, Person)) {
+    _cmnRemoveAllErrorMessage();
+    var TotalBill, TotalTips, PerPersonTips;
+
+    TotalTips = (Tips / 100) * Bill;
+    TotalBill = TotalTips + Bill;
+    PerPersonTips = TotalBill / Person;
+
+    _cmnHideElement("OutputInfo"); //will hide our basic div
+    _cmnShowElement("OutputResult", "flex"); //will show our result div
+
+    // document.getElementById("bill").innerHTML = Bill;
+    // document.getElementById("tip").innerHTML = Tips;
+    // document.getElementById("person").innerHTML = Person;
+
+    SetTipResult("bill", Bill);
+    SetTipResult("tip", Tips);
+    SetTipResult("person", Person);
+    SetTipResult("totalTips",TotalTips);
+    SetTipResult("totalBill", TotalBill);
+    SetTipResult("perPersonBill", PerPersonTips);
+  }
+}
+
+function ResetTipCalculator() {
+    if (confirm("Are you Sure Want to reset the calculator?")) {
+      document.getElementById("inputBill").value = "";
+      document.getElementById("inputTips").value = "";
+      document.getElementById("inputPerson").value = "";
+
+      _cmnRemoveAllErrorMessage();
+      _cmnHideElement("OutputResult"); //will hide our OutputResult div
+      _cmnShowElement("OutputInfo", "flex"); //will show our OutputInfo div
+
+      SetTipResult("bill","0");
+      SetTipResult("tip","0");
+      SetTipResult("person","0");
+      SetTipResult("totalTips","0");
+      SetTipResult("totalBill","0");
+      SetTipResult("perPersonBill","0");
+    }
+  }
+
+function SetTipResult(elementId, value) {
+  document.getElementById(elementId).innerHTML = value;
+}
+
+function ValidateTipCalculatorForm(Bill, Tips, Person) {
+  if (Bill <= 0 || _cmnIsInputFieldEmpty(Bill)) {
+    _cmnShowErrorMessageBottomOfTheInputField( "inputBill","Please enter the valid Bill.");
+    // alert("Please enter valid Bill.");
+    return false;
+  }
+
+  if (Tips <= 0  || _cmnIsInputFieldEmpty(Tips)) {
+    _cmnShowErrorMessageBottomOfTheInputField("inputBill", "Please enter the valid Tip."
+    );
+    // alert("Please enter valid Tip.");
+    return false;
+  }
+
+  if (Person <= 0 || _cmnIsInputFieldEmpty(Person)) {
+    _cmnShowErrorMessageBottomOfTheInputField( "inputBill","Please enter the valid Person."
+    );
+    // alert("Please enter valid Person.");
+    return false;
+  }
+  return true;
+}
+
+
